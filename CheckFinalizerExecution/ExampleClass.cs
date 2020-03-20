2ea384e0b508d2ebf8c32d9001e3052d05884241
Volodymyr Tsurko <chegnenado@yahoo.com>
2020-03-20 10:43:17 +0200

    Added demo from "Object.Finalize()" MS Docs article.

---
+++ CheckFinalizerExecution/ExampleClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CheckingFinalizerExecution
+{
+    class ExampleClass
+    {
+        private Stopwatch sw;
+
+        public ExampleClass()
+        {
+            sw = Stopwatch.StartNew();
+            Console.WriteLine("Instantiated object");
+        }
+
+        public void ShowDuration()
+        {
+            Console.WriteLine($"This instance of {this} has been in existence for {sw.Elapsed}");
+        }
+
+        ~ExampleClass()
+        {
+            Console.WriteLine("Finalizing object");
+            sw.Stop();
+            ShowDuration();
+        }
+    }
+}
---
+++ CheckFinalizerExecution/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CheckingFinalizerExecution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateLocalExampleClassInstance();
+
+            // Forcing the collection. Alternatively, you can see the moment the application terminates (run this program in the command interpreter).
+            GC.Collect();
+
+            Console.ReadLine();
+        }
+
+        private static void CreateLocalExampleClassInstance()
+        {
+            var example = new ExampleClass();
+            example.ShowDuration();
+        }
+    }
+}
